ba70e6bcd2cb11e55fbdecce54cf51e7bb9e5885
kulkarnipallavis <kulkarnipallavis@gmail.com>
2016-11-11 18:00:52 -0500

    Querying with Models

---
+++ models/index.js
@@ -0,0 +1,45 @@
+const Sequelize = require('sequelize');
+const db = new Sequelize('postgres://localhost:5432/wikistack', {
+	logging : true;
+});
+
+const Page = db.define('page', {
+    title: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    urlTitle: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    content: {
+        type: Sequelize.TEXT,
+        allowNull: false
+    },
+    status: {
+        type: Sequelize.ENUM('open', 'closed')
+    },
+    date: {
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.NOW
+    },
+    route : {
+    	type: DataTypes.Virtual
+    }
+});
+
+const User = db.define('user', {
+    name: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    email: {
+        type: Sequelize.STRING,
+        allowNull: false
+    }
+});
+
+module.exports = {
+  Page: Page,
+  User: User
+};
---
+++ routes/index.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+const bodyParser = require('body-parser');
+
+
+module.exports = function(server){
+
+}
